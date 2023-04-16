24089672b2616c0b0c27715f1669fcc5574f996b
Sachin Kumar Rajput <31756244+skrmain@users.noreply.github.com>
2023-04-16 17:30:50 +0000

    fixes

--- src/apps/product/routes.ts
+++ src/apps/product/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import {
     addProduct,
+    addSampleProducts,
     deleteProduct,
     getProduct,
     getProductImage,
@@ -14,14 +15,15 @@ import { checkAuth, handleFiles } from '../../shared/middleware';
 
 const router = Router();
 
-router.get('/', getProducts);
-router.get('/:productId', getProduct);
-router.post('/', checkAuth, handleFiles, addProduct);
+router.route('/').get(getProducts).post(checkAuth, handleFiles, addProduct);
+
+router.route('/:productId').get(getProduct).patch(checkAuth, updateProduct).delete(checkAuth, deleteProduct);
 router.get('/:productId/images/:imageId/:imageName', getProductImage);
-router.patch('/', checkAuth, updateProduct);
-router.delete('/', checkAuth, deleteProduct);
-router.put('/:productId/save', checkAuth, saveProduct);
-router.delete('/:productId/save', checkAuth, removeProduct);
-router.post('/add-sample-products', checkAuth);
+
+// NOTE: have it here only, but if we have Model which is used for multiple Models then have in respective route file
+// - like in case of otp-logs model -- that can be used for (user, business) {email, mobile}  verification
+router.route('/:productId/save').all(checkAuth).put(saveProduct).delete(removeProduct);
+
+router.post('/add-sample-products', checkAuth, addSampleProducts);
 
 export default router;
