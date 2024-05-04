51bdfa890a94536234378233472ba4fd9c81a086
Sachin Kumar Rajput <31756244+skrmain@users.noreply.github.com>
2024-05-04 17:49:20 +0000

    Add signin with google
    add authorize-url, callback setup

--- src/apps/auth/controller.ts
+++ src/apps/auth/controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express';
+import { OAuth2Client } from 'google-auth-library';
 
 import userService from '../user/user.service';
 
 import { InvalidHttpRequestError, UnauthorizedHttpRequestError } from '../../shared/custom-errors';
 import { ILoginBody, IRegisterBody } from './types';
 import { createToken, successResponse, verifyToken } from '../../shared/utils';
+import { googleOAuthCred } from '../../config';
 
 const register = async (req: Request<any, any, IRegisterBody>, res: Response) => {
     const details = req.body;
@@ -61,6 +63,49 @@ const refreshAccessToken = async (req: Request, res: Response) => {
     return res.send(successResponse({ data: { token } }));
 };
 
+// TODO:
+const getAuthorizeUrl = async (req: Request, res: Response) => {
+    const oAuthClient = new OAuth2Client({
+        clientId: googleOAuthCred.web.client_id,
+        clientSecret: googleOAuthCred.web.client_secret,
+        redirectUri: googleOAuthCred.web.redirect_uris[0],
+    });
+
+    const authorizeUrl = oAuthClient.generateAuthUrl({
+        access_type: 'offline',
+        scope: ['https://www.googleapis.com/auth/userinfo.profile'],
+    });
+
+    return res.send(successResponse({ data: { authorizeUrl } }));
+};
+
+// TODO:
+const callback = async (req: Request, res: Response) => {
+    console.log('Req.Query', req.query);
+    const { code } = req.query;
+
+    if (!code) throw new Error('"code" does not exists in query');
+
+    const oAuthClient = new OAuth2Client({
+        clientId: googleOAuthCred.web.client_id,
+        clientSecret: googleOAuthCred.web.client_secret,
+        redirectUri: googleOAuthCred.web.redirect_uris[0],
+    });
+
+    const re = await oAuthClient.getToken(code.toString());
+    console.log('R ', re);
+
+    oAuthClient.setCredentials(re.tokens);
+
+    if (!re.tokens.access_token) {
+        throw new Error("access_token doesn't exists");
+    }
+
+    const tokenInfo = await oAuthClient.getTokenInfo(re.tokens.access_token);
+
+    return res.send(successResponse({ message: 'Success' }));
+};
+
 export default {
     register,
     login,
@@ -68,4 +113,6 @@ export default {
     setPassword,
     activateAccount,
     refreshAccessToken,
+    getAuthorizeUrl,
+    callback,
 };
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,24 @@
+import { config } from 'dotenv';
+
+config();
+
 export default {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 8000,
     mongodbUri: process.env.MONGODB_URI || 'mongodb://admin:admin@localhost:27017',
     jwtSecret: process.env.JWT_SECRET || 'secret',
     encryptionSecret: process.env.ENCRYPTION_SECRET || 'secret',
+};
+
+export const googleOAuthCred = {
+    web: {
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        project_id: process.env.GOOGLE_PROJECT_ID,
+        auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+        token_uri: 'https://oauth2.googleapis.com/token',
+        auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        redirect_uris: ['http://localhost:8000', 'http://localhost:3000'],
+        javascript_origins: ['http://localhost:8000', 'http://localhost:3000'],
+    },
 };
